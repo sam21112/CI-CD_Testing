1a08b2192e37799deefcb7f918e5ee07e00cdae9
samarth raipal <samarthraipal@gmail.com>
2024-11-30 23:31:28 -0500

    added test cases

---
+++ test/test_operation.py
@@ -0,0 +1,9 @@
+from src.math_operations import add,substract
+
+def test_add():
+        assert(2,3)==5
+        assert(-1,1)==0
+def test_subtract():
+        assert(2,3)==-1
+        assert(-1,1)==-2
+        
